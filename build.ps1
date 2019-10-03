93e6a557edbddbcb3fde7e2881a87e703c201a96
Patrick Lioi <patrick.lioi@gmail.com>
2019-10-02 21:39:49 -0500

    Target the netcoreapp3.0 framework, pinning to the corresponding 3.0.100 SDK.

--- build.ps1
+++ build.ps1
@@ -8,8 +8,8 @@ main {
     mit-license $copyright
     exec { dotnet --version }
 
-    exec { dotnet clean src -c $configuration /nologo -v minimal }
-    exec { dotnet build src -c $configuration /nologo }
+    exec { dotnet clean src -c $configuration --nologo -v minimal }
+    exec { dotnet build src -c $configuration --nologo }
 
     exec { dotnet fixie --configuration $configuration --no-build } src/Fixie.Tests
     exec { dotnet test --configuration $configuration --no-build } src/xUnit.Tests

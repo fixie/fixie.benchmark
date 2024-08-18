74c0ca0de8ac9dd50a3d621b54269e02bcaeab96
Patrick Lioi <patrick.lioi@gmail.com>
2024-08-17 21:42:29 -0500

    Allow Fixie packages to be fetched from nuget.org

---
+++ src/Fixie.Tests/TestProject.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+
+namespace Fixie.Tests;
+
+class TestProject : ITestProject
+{
+    public void Configure(TestConfiguration configuration, TestEnvironment environment)
+    {
+        configuration.Conventions.Add<DefaultDiscovery, ParallelExecution>();
+    }
+
+    class ParallelExecution : IExecution
+    {
+        public async Task Run(TestSuite testSuite)
+        {
+            await Parallel.ForEachAsync(testSuite.Tests, async (test, _) => await test.Run());
+        }
+    }
+}

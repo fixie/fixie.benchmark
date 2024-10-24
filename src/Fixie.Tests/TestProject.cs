3d5aa745d3d137d8aece789584a0884b22ddf379
Patrick Lioi <patrick.lioi@gmail.com>
2024-10-24 16:13:08 -0500

    Update Fixie to 4.0.0.

--- src/Fixie.Tests/TestProject.cs
+++ src/Fixie.Tests/TestProject.cs
@@ -1,19 +1,9 @@
-﻿using System.Threading.Tasks;
-
-namespace Fixie.Tests;
+﻿namespace Fixie.Tests;
 
 class TestProject : ITestProject
 {
     public void Configure(TestConfiguration configuration, TestEnvironment environment)
     {
         configuration.Conventions.Add<DefaultDiscovery, ParallelExecution>();
-    }
-
-    class ParallelExecution : IExecution
-    {
-        public async Task Run(TestSuite testSuite)
-        {
-            await Parallel.ForEachAsync(testSuite.Tests, async (test, _) => await test.Run());
-        }
     }
 }
